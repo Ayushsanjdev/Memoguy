42d5c938ec3bbcb4f5c459f4652a53a706cba936
ayushsanjdev <ayushsanjpro@gmail.com>
2021-06-20 19:47:02 +0530

    add: firebase add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
-import Title from './components/Head';
+import Head from './components/Head';
 import LeftSideBar from './components/LeftSideBar';
 import RightSideBar from './components/RightSideBar';
+import db from './firebase/config';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 const App = () => {
 
+  const [showTitle, setShowTitle] = useState('');
+
+  // useEffect(() => {
+  //   getData()
+  // }, [])
+
+  const getData = () => {
+    db.collection("notes").get().then((doc) => {
+    if(doc.exists) {
+      console.log("doc data: ", doc.data());
+    } else {
+      console.log("no such doc");
+    }
+  }).catch((error) => {
+    console.log("error getting doc: ", error)
+  })}
+
+
+  const addTitle = (input) => {
+    firebase.firestore().collection("notes").add({
+    title: input,
+    createdAt: firebase.firestore.FieldValue.serverTimestamp()
+  })
+  .then(() => {
+    console.log("doc successfully written!");
+  })
+  .catch((error) => {
+    console.error("error: ", error)
+  })}
+
   return (
     <div className="App">
-      <Title/>
+      <Head setShowTitle={setShowTitle} addTitle={addTitle} />
       <main>
-        <LeftSideBar/>
+        <LeftSideBar showTitle={showTitle} />
         <RightSideBar/>
       </main>
       <footer>
-        Made with 💕 by 
+        Made with 🧠 by 
         <a href="https://github.com/ayushsanjdev"> ayushsanjdev</a>
       </footer>
     </div>
