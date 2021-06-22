ca75853c7f1ee5e1f15f58039bbc6b4f4da473a4
ayushsanjdev <ayushsanjpro@gmail.com>
2021-06-22 11:11:20 +0530

    refactor: var name

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import SpringModal from "./modal";
 
-const Head = ({ setShowNotes, addData }) => {
+const Head = ({ setShowTitle, showTitle, addData }) => {
   return (
     <header>
       <div className="open">
         <h1>Memoguy </h1>
         <small> open</small>
       </div>
-      <SpringModal setShowNotes={setShowNotes} addData={addData} />
+      <SpringModal setShowTitle={setShowTitle} showTitle={showTitle} addData={addData} />
     </header>
   );
 };
