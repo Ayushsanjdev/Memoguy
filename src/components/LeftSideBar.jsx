dee6e7805c2a7025b2ff91d848b05279138a5c79
ayushsanjdev <ayushsanjpro@gmail.com>
2021-06-23 10:13:30 +0530

    add: onclick selector title color

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import { Divider } from "@material-ui/core";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 const LeftSideBar = ({ allNotes, delData }) => {
 
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  }
+
   return(
     <div className="leftsidebar">
     {allNotes.map((title) => (
-      <ListItem button key={title.id} className="list">
+      <section key={title.id}>
+      <ListItem button className="list" selected={selectedIndex === title.id}
+          onClick={(event) => handleListItemClick(event, title.id)}>
         <ListItemText primary={title.title} />
         <DeleteIcon className="deleteIcon" onClick={delData} />
       </ListItem>
+      <Divider></Divider>
+      </section>
     ))}
     </div>
   ) 
