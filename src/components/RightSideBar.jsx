221ce337a8e7b58c5c3d25a26db5ea97bcbcb5ad
ayushsanjdev <ayushsanjpro@gmail.com>
2021-06-26 12:40:55 +0530

    add: right bar function and props & refactor

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactQuill from "react-quill";
 
-const RightSideBar = ({ setShowBody, showBody, allNotes, selectedNote }) => {
+const RightSideBar = ({
+  setShowBody,
+  showBody,
+  setShowTitle,
+  allNotes,
+  selectedNote,
+  updateData,
+}) => {
   // const addBody = async (val) => {
   //   await setShowBody(val);
   // };
 
+  const updateTitle = (e) => {
+    e.preventDefault();
+    setShowTitle(e.target.value);
+  };
+
   return (
     <div className="editor">
+        <input
+          id="title"
+          placeholder="title"
+          className="quillTitle"
+          onChange={updateTitle}
+        />
       <ReactQuill theme="snow" className="reactQuill"></ReactQuill>
     </div>
   );
