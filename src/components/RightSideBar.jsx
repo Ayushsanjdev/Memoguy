da87eb67f827800b515891239d2030531f8f6abd
ayushsanjdev <ayushsanjpro@gmail.com>
2021-07-04 19:56:19 +0530

    refactor

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { convertToRaw, EditorState, ContentState} from "draft-js";
+import { convertToRaw, EditorState, ContentState}
+ from "draft-js";
 import createHashtagPlugin from '@draft-js-plugins/hashtag';
 import createLinkifyPlugin from '@draft-js-plugins/linkify';
 import InlineStylesEditor from "./EditorStyles";
@@ -22,38 +23,31 @@ const RightSideBar = ({
   showTitle,
 }) => {
 
-  const [editorState, setEditorState] = useState
-    (
-      selectedNoteBody === null ? 
-      EditorState.createEmpty()
-      :
-      EditorState.createWithContent(ContentState.
-        createFromText(selectedNoteBody)) // now editorstate is selctedNoteBody's text
-    );
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty());
 
   const updateTitle = (e) => {
     e.preventDefault();
     setSelectedNote(e.target.value)
   };
 
-  const updateBody = (content) => {
-    const stringContent = JSON.stringify(
-      convertToRaw(content));
-    const textContent = JSON.parse(stringContent);   // parsing json and accessing the text on nextline  
-    setSelectedNoteBody(textContent.blocks[0].text);
-  }
-
   const handleBody = (editorState) => {
     const contentState = editorState.getCurrentContent();
-    updateBody(contentState);
-    setEditorState(editorState);
+    const stringContent = JSON.stringify(
+      convertToRaw(contentState));
+    const textContent = JSON.parse(stringContent);
+    // const mainText = textContent.blocks[0].text;
+    setEditorState(textContent);
+    setSelectedNoteBody(textContent);
   }
+
+  
 
   return (
     <div className="editor">
 
       <div className="editorTitle">
-        <input type="text" 
+        <input type="text"
           className="editorTitle"
           value={selectedNote} 
           onChange={updateTitle} />
